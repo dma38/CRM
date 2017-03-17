cb1eb08606957723be3ab38804cf5b62a8b263f4
Di Ma <tarynma0204@yahoo.ca>
2017-03-17 05:08:19 +0000

    part2 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'customers/alphabetized' => 'finder#alphabetized'
+
+  get 'customers/missing_email' => 'finder#missing_email'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'finder#index'
 end
